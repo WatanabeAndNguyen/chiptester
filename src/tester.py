b5d9223b9141185fb525566d9eac34bf8c90c719
leandrohw <leandrohw@gmail.com>
2016-05-21 05:00:16 +0000

    Setup spi clock and global clock and run them in parallel

--- src/tester.py
+++ src/tester.py
@@ -1,44 +1,70 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 ## Global variables
-# Values
+# States
+isActive = True
+# Clocks
 clkVal = False
 spiVal = False
+# Data
 resoln = 0
 direcn = 0
 stpCnt = 1
 # Pins
 clkPin = 11
 swtPin = 12
+spiPin = 13
 # Delays
-clkDel = 2
-spiDel = 3
+clkDel = 0.5
+spiDel = 1.3
 
-## This sets mode to BOARD, which means we refer to pin rather than GPIO
-#GPIO.setmode(GPIO.BOARD)
-## Set the clock pin as an output
-#GPIO.setup(clkPin, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
-## Set the switch pin as an output
-#GPIO.setup(swtPin, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+## Sets up the board
+def setup():
+    ## This sets mode to BOARD, which means we refer to pin rather than GPIO
+    GPIO.setmode(GPIO.BOARD)
+    ## Set the clock pin as an output
+    GPIO.setup(clkPin, GPIO.OUT)
+    ## Set the spi clock pin as an output
+    GPIO.setup(spiPin, GPIO.OUT)
+    ## Set the switch pin as an output
+    GPIO.setup(swtPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #GPIO.add_event_detect(swtPin, GPIO.RISING, callback=toggleSwitch, bouncetime=300)
 
-## Do something when button is pressed
-def toggleSwitch(pin):
+
+    
+def mycallback(channel):
+    global clkVal, clkPin
+    GPIO.output(clkPin, clkVal)
+    clkVal = not(clkVal)
+        
+def toggleSwitch():
+    global swtPin
     while True:
-        if GPIO.input(pin) == GPIO.LOW:
-            print("Switch pressed.")
-            break
+        if GPIO.input(swtPin) == GPIO.HIGH:
+            GPIO.output(clkPin, True)
+        else:
+            GPIO.output(clkPin, False)
 
 ## Generate clock signal for the chip
-def clock(pin, delay):
-    global clk
-    while True:
-        GPIO.output(pin, TRUE)
+def global_clock(pin, delay):
+    global clkVal
+    while isActive:
+        GPIO.output(pin, clkVal)
         time.sleep(delay)
-        clk = not(clk)
+        clkVal = not(clkVal)
+
+## Generate clock signal for the chip
+def spi_clock(pin, delay):
+    global spiVal
+    while isActive:
+        GPIO.output(pin, spiVal)
+        time.sleep(delay)
+        spiVal = not(spiVal)
 
 ## Generate spi signal for the chip
-def spi(spiPin, csPin, resololution, direction, count):
+def spi(spiPin, csPin, resolution, direction, count):
     binary = inputToBinary(resolution, direction, count)
     GPIO.output(csPin, True)
     GPIO.output(csPin, False)
@@ -55,9 +81,25 @@ def inputToBinary(resolution, direction, count):
     return(binary)
 
 
-print(inputToBinary(0,0,3))
-# GPIO.cleanup()
 
-#    thread = Thread(target = toggleSwitch)
-#    thread.start()
-#    thread.join()
+
+if __name__ == '__main__':
+    print("Initialization\n")
+    setup()
+    try:
+        #clock(clkPin, clkDel)
+        #toggleSwitch()
+        print("Running code\n")
+        clkThread = threading.Thread(target=global_clock, args=(clkPin, clkDel))
+        spiThread = threading.Thread(target=spi_clock, args=(spiPin, spiDel))
+        clkThread.start()
+        spiThread.start()
+        clkThread.join()
+        spiThread.join()
+    except KeyboardInterrupt:
+        isActive = False
+        print("Cleaning up\n")
+        GPIO.cleanup()
+        print("Done\n")
+
+
